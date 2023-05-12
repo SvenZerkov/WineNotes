f3650bd9ca3ae2b74a9a7bbc081311d83f54d47c
SanteriV <54982640+SvenZerkov@users.noreply.github.com>
2023-05-12 09:36:01 +0300

    baseline initialization

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const mongoose = require("mongoose");
+const exhbs = require("express-handlebars");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+const ObjectId = mongoose.Types.ObjectId;
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("", require("./routes/routes.js"));
+app.engine("handlebars", exhbs.engine({
+    defaultLayout: "main",
+    partialsDir: path.join(__dirname, "views/partials")
+}));
+app.use(express.json());
+app.set("view-engine", "handlebars");
+
+const routes = require("./routes/routes");
+
+const dbURI = process.env.dbURI;
+
+const client = new MongoClient(dbURI, {useNewUrlParser: true });
+
+const connectToDB = async () => {
+    try {
+        await mongoose.connect(dbURI);
+        console.log("Database connected");
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => console.log(`Server up and running. Listening port ${PORT}`));
+    } catch (error) {
+        console.log(error); // change this later to log in file
+    }
+};
+
+connectToDB();
